f48c719350f7427ccab9260381553cecf86659ec
Yasas <wayasas@gmail.com>
2018-05-27 17:46:06 +0530

    update referance handeling with the menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { HomePage } from '../pages/home/home';
 import { SettingsPage } from '../pages/settings/settings';
 import { AboutUsPage } from '../pages/about-us/about-us';
 import { SubmitPage } from '../pages/submit/submit';
+import { TimetablePage } from '../pages/timetable/timetable';
+import { AlertsPage } from '../pages/alerts/alerts';
 
 @Component({
   templateUrl: 'app.html'
@@ -16,19 +18,19 @@ export class MyApp {
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: { [title: string]: any };
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'Submit', component: SubmitPage },
-      { title: 'Settings', component: SettingsPage },
-      { title: 'About Us', component: AboutUsPage },
-    ];
-
+    this.pages = {};
+    this.pages['Home'] = HomePage;
+    this.pages['Alerts'] = AlertsPage;
+    this.pages['Submit'] = SubmitPage;
+    this.pages['Settings'] = SettingsPage;
+    this.pages['Timetable'] = TimetablePage;
+    this.pages['About Us'] = AboutUsPage;
   }
 
   initializeApp() {
@@ -43,6 +45,6 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    this.nav.setRoot(this.pages[page]);
   }
 }
