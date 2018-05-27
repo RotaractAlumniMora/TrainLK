5257f85aea1e1ff0baff04881a1e2ac8b656d6e6
Yasas <wayasas@gmail.com>
2018-05-27 18:54:36 +0530

    add functionality to save internal settings in storage

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html',
 })
 export class SettingsPage {
+  preferred_line: string;
 
-  constructor(public navCtrl: NavController) {
+  show_notifications: boolean;
 
+  constructor(public navCtrl: NavController, private storage: Storage) {
+    this.loadSettings()
   }
 
+  saveSettings() {
+    this.storage.set('preferred_line', this.preferred_line);
+    this.storage.set('show_notifications', this.show_notifications);
+  }
+
+  loadSettings() {
+    this.storage.get('preferred_line').then((val) => {
+      this.preferred_line = val;
+    });
+    this.storage.get('show_notifications').then((val) => {
+      this.show_notifications = val;
+    });
+  }
 }
