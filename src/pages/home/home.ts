84ca3c429f3c10bbad5c29b40ad48f3b39fbe8c8
Yasas <wayasas@gmail.com>
2018-05-28 11:04:09 +0530

    link view page with home news cards

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ViewNewsPage } from '../view-news/view-news';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  openNews(id) {
+    this.navCtrl.push(ViewNewsPage, {'newsId': id});
   }
 
 }
