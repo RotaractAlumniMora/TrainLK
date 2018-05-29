8c925dbc6c6f98bf0c3fd74ca1a8d9d19d01c73d
Vindulamj <vindulamj@gmail.com>
2018-05-29 08:53:14 +0530

    Add sample alert page

--- src/pages/alerts/alerts.ts
+++ src/pages/alerts/alerts.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams,  AlertController, ToastController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
 
 /**
  * Generated class for the AlertsPage page.
@@ -13,12 +14,23 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'alerts.html',
 })
 export class AlertsPage {
+  hideMe: boolean;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public toastCtrl: ToastController, public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AlertsPage');
   }
+
+  submitForm() {
+    let loader = this.loadingCtrl.create({
+      content: "Alerts Loading...",
+      duration: 3000
+    });
+    loader.present();
+    this.hideMe = true;
+  }
+
 
 }
